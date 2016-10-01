5f38b830a4eea2639691b695612a4525c840e41b
Arunkumar S <asrisailapathi@vmware.com>
2016-10-01 18:42:25 +0530

    Additional implementation of recent searches

--- static/js/answer.js
+++ static/js/answer.js
@@ -1,6 +1,49 @@
 // Uses Tree lookup API to traverse the Tree in Bread First manner
 
 var Steps = Steps || {};
+var Storage = Storage || {};
+
+Storage = (function () {
+    var fn = {
+        _setItem: function (key, value, isCloud) {
+            if(window.localStorage) {
+                window.localStorage.setItem(key, value);
+            }
+            else {
+                //document.cookie = key + '=' + value;
+            }
+        },
+        _getItem: function (key, isCloud) {
+            if(window.localStorage) {
+                return window.localStorage.getItem(key);
+            }
+            else {
+                //document.cookie = key + '=' + value;
+            }
+        },
+        _removeItem: function (key) {
+            if(window.localStorage) {
+                window.localStorage.removeItem(key);
+            }
+            else {
+                //document.cookie = key + '=' + value;
+            }
+        }
+    };
+    var api = {
+        setItem: function (key, value) {
+            return fn._setItem.apply(this, arguments);
+        },
+        getItem: function (key) {
+            return fn._getItem.apply(this, arguments);
+        },
+        removeItem: function(key) {
+            return fn._removeItem.apply(this, arguments);
+        }
+    };
+    return api;
+})();
+
 
 Steps = (function () {
     var newNodes = [];
@@ -9,6 +52,9 @@ Steps = (function () {
     var lookNode = 1;
     
     var visit = [];
+    
+    var recents = {};
+    var shownRecents = [], recentCounter = 0;
     
     var result, showResult;
     
@@ -29,17 +75,20 @@ Steps = (function () {
             cache['button'].addEventListener('click', fn._handleClick);
         },
         _showError: function () {
+            var parent = document.getElementsByClassName('error-area')[0];
+            parent.innerHTML = '';
             var error = document.createElement('div');
             error.classList.add('error-class');
-            var parent = document.getElementsByClassName('inner')[0];
+            
             error.innerHTML = 'Enter only numbers';
             parent.appendChild(error);
             cache['text'].value = '';
             setTimeout(function () {
-                parent.removeChild(error);
+                parent.innerHTML = '';
             }, 2000);
         },
         _validateInput: function (value) {
+            if(value === '') return true;
             return isNaN(value);
         },
         _handleClick: function () {
@@ -51,19 +100,45 @@ Steps = (function () {
             if(!cache['text'])
                 cache['text'] = document.getElementById('search');
             lookNode = cache['text'].value;
+            lookNode = lookNode.trim();
             if(fn._validateInput(lookNode)) {
                 fn._showError();
                 return;
             }
-            
-            var promise = new Promise(fn._traverseTree);
-            promise.then(function (val) {
-               console.log('Fulfilled', root); 
-                result.innerHTML = 'The number: <span class="bold_class">' + lookNode + '</span> that you have searched is found at: <span class="success_message bold_class">' + root + '</span>';
-            }).catch(function(reason) {
-                console.log('Not found');  
-                result.innerHTML = 'The number: ' + lookNode + ' that you have searched is not found';
-            });
+            var hasSearched = recents[lookNode];
+            if(hasSearched) {
+                fn._displayMessage(hasSearched !== null, hasSearched);
+            }
+            else {
+                var promise = new Promise(fn._traverseTree);
+                console.log ('Using promise');
+                promise.then(function (val) {
+                    recents[lookNode] = root;
+                    fn._displayMessage(true, root);
+                }).catch(function(reason) {
+                    recents[lookNode] = null;  
+                    fn._displayMessage(false, null);
+                });
+            }        
+        },
+        _updateRecents: function () {
+            var recentElem = document.createElement('span');
+            var parent = document.getElementById('recent');
+            var keys = Object.keys(recents);
+            var len = keys.length;
+            parent.innerHTML = '';
+            for(var i = 0; i < len; ++ i) {
+                (function (i) {
+                    recentElem = document.createElement('span');
+                    recentElem.classList.add('recent-item');
+                    recentElem.innerText = keys[i] + ' -> ' + recents[keys[i]];
+                    parent.appendChild(recentElem);
+                })(i);
+            }
+        },
+        _displayMessage: function (isFound, position) {
+            result.innerHTML = 'The number: ' + lookNode + ' that you have searched is ' + (isFound ? 'found at: <span class="success_message bold_class">' + position + '</span>' : 'not found');
+            fn._updateRecents();
         },
         _traverseTree: function (resolve, reject) {
             root = '/';
